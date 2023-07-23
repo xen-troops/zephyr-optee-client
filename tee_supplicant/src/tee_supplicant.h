c3d7f29560286353d4ce25353ff5eeee4748598a
Dmytro Semenets <dmytro_semenets@epam.com>
2023-07-23 14:07:47 +0300

    tee_supplicant: Use shm registry in file operations
    Use memory registry in file file operations.
    
    Signed-off-by: Dmytro Semenets <dmytro_semenets@epam.com>
    Reviewed-by: Sergiy Kibrik <Sergiy_Kibrik@epam.com>
    Acked-by: Oleksii Moisieiev <oleksii_moisieiev@epam.com>
    Acked-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

--- tee_supplicant/src/tee_supplicant.h
+++ tee_supplicant/src/tee_supplicant.h
@@ -6,6 +6,13 @@
 #ifndef TEE_SUPPLICANT_H
 #define TEE_SUPPLICANT_H
 
+/* Helpers to access memref parts of a struct tee_param */
+#define MEMREF_SHM_ID(p)          ((p)->c)
+#define MEMREF_SHM_OFFS(p)        ((p)->a)
+#define MEMREF_SIZE(p)            ((p)->b)
+#define SET_MEMREF_SHM_ID(p, v)   ((p)->c = (v))
+#define SET_MEMREF_SIZE(p, v)     ((p)->b = (v))
+
 void *tee_param_get_mem(struct tee_param *param, size_t *size);
 
 #endif /* TEE_SUPPLICANT_H */
